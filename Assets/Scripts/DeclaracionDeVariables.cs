78322ab21a4c27815e0c7453281f28ec1d4c6442
micabod <167199490+micabod@users.noreply.github.com>
2025-04-01 08:43:23 -0300

    ejercicio 1 terminado

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -9,10 +9,19 @@ public class DeclaracionDeVariables : MonoBehaviour
     string nombreAlumno;
     bool estaAprobado;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cantidadAlumnos = 32;
+        kilosVendidos = 23.7f;
+        nombreAlumno = "Jeronimo";
+        estaAprobado = false;
+        Debug.Log(cantidadAlumnos);
+        Debug.Log(kilosVendidos);
+        Debug.Log(nombreAlumno);
+        Debug.Log(estaAprobado);
+
     }
 
     // Update is called once per frame
